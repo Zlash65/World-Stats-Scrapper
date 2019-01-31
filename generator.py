5e929576e71a96a5f8d793340de05e7a1dad83b5
Zlash65 <zarrar65@gmail.com>
2019-01-31 09:56:38 +0530

    logic to scrape and clean data added

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
-import json
+from bs4 import BeautifulSoup
+import requests, json
 import pymysql
 from pymysql import OperationalError
 
@@ -28,6 +29,33 @@ def validate_db_credentials():
 	except OperationalError as e:
 		print("Cannot establish conncetion to database. Make sure the credentials are right.")
 		raise OperationalError
+
+def scrape_and_retrieve_data():
+	# url that we are targetting for this specific scraping
+	url_to_scrape = "https://www.internetworldstats.com/list2.htm"
+
+	# use beautifulsoup to get the html content of the site
+	page_response = requests.get(url_to_scrape, timeout=5)
+	page_content = BeautifulSoup(page_response.content, "html.parser")
+
+	# data that we want to retrieve is stored in table with attribute summary=data2
+	data_table = [d for d in page_content.find_all('table') if d.get("summary") == "data2"]
+
+	# retrieve headers from the first row element
+	headers = [k.text for k in data_table[0].find_all('tr')[0].find_all('b')]
+	headers = [k.replace('\r\n', ' ') if '-' not in k else k.replace('\r\n-', '') for k in headers]
+
+	# retrieve data that falls under the required headers
+	data = []
+	for i in data_table:
+		for j, k in enumerate(i.find_all('tr')):
+			if j == 0: continue # ignore headers row
+			temp = list(filter(None, k.text.replace('\r\n%', ' %').replace('\r\n', '').split('\n')))
+			if temp[1]=="--": temp[1] = temp[0]
+			data.append(temp)
+
+	globals()["headers"] = headers
+	return headers, data
 
 if __name__ == "__main__":
 	# validate credentials for database
